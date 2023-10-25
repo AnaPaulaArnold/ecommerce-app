4d147dcc8b11ef0573bb251d6e56afd84aa51ccc
Ana Arnold <anapaula.abreuarnold@gmail.com>
2023-10-24 22:53:25 -0300

    added prettier configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-
-const App = ()=> {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>testes</h1>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>testes</h1>
+			</header>
+		</div>
+	);
+};
 
 export default App;
